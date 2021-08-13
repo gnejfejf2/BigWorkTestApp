766104aa613721a83b41d5ac9e839cdf6c324a47
강지윤 <gnejfejf3@naver.com>
2021-08-13 12:57:14 +0900

    Initial Commit

---
+++ BigWorkTestApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BigWorkTestApp
+//
+//  Created by kang jiyoun on 2021/08/13.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
